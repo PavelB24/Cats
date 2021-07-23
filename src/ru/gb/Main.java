cdae46d7c709587b01a224f66b03846c82a6535e
Pavel <alkatrazzzzz@mail.ru>
2021-07-23 18:35:11 +0300

    v2

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -1,15 +1,29 @@
 package ru.gb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Cat cat = new Cat(5, "jojo");
-        Plate plate1 = new Plate ("Jojos plate", 100);
+        Plate plate1 = new Plate ("Jojos plate", 15);
         Human owner = new Human("Паша");
         cat.toEat(plate1);
         System.out.println(cat.getSatiety());
         System.out.println(plate1.getInfo());
-        owner.toFillThePlate(plate1, 100);
+        owner.toFillThePlate(plate1, 1);
         System.out.println(plate1.getInfo());
+        List<Cat> catList=new ArrayList<>();
+        catList.add(cat);
+        catList.add(new Cat(3, "Bongo"));
+        catList.add(new Cat(8, "Вася"));
+        for (int i = 0; i <catList.size() ; i++) {
+            System.out.println();
+            catList.get(i).toEat(plate1);
+            System.out.println("Котик сыт? "+catList.get(i).getSatiety()+ ", миска заполнена на " +plate1.getInfo()+ "%");
+
+
+        }
     }
 }
